e49633d2e67c262d2e7fd11e34bad7729f85ad13
JoelPoah <joel.21@ichat.sp.edu.sg>
2022-06-29 23:13:20 +0800

    first react web

---
+++ build/precache-manifest.c3f2160cd37ed1dce5147569ec934c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec2a688255e8539cbfa75020a6de5d1f",
+    "url": "/JoelPoah/FirstReact.github.io.git/index.html"
+  },
+  {
+    "revision": "ac58ec08f4ae12f34d06",
+    "url": "/JoelPoah/FirstReact.github.io.git/static/css/main.e6cdf681.chunk.css"
+  },
+  {
+    "revision": "556e470fb06602a69831",
+    "url": "/JoelPoah/FirstReact.github.io.git/static/js/2.1d916e84.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/JoelPoah/FirstReact.github.io.git/static/js/2.1d916e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac58ec08f4ae12f34d06",
+    "url": "/JoelPoah/FirstReact.github.io.git/static/js/main.f84ab9a9.chunk.js"
+  },
+  {
+    "revision": "fe2c682cf0560ef39b1a",
+    "url": "/JoelPoah/FirstReact.github.io.git/static/js/runtime-main.f5c997cd.js"
+  }
+]);
