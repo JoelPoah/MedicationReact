1352feafb3f6789e9562dc7d8b710266e4d8318e
JoelPoah <joel.21@ichat.sp.edu.sg>
2022-06-29 23:19:37 +0800

    Updates

---
+++ precache-manifest.703aef3c6f158c98fe1ef3e63b14f1fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1c7c65e6431d3569b75bc3746301f31",
+    "url": "/JoelPoah.github.io/MedicationReact.git/index.html"
+  },
+  {
+    "revision": "ac58ec08f4ae12f34d06",
+    "url": "/JoelPoah.github.io/MedicationReact.git/static/css/main.e6cdf681.chunk.css"
+  },
+  {
+    "revision": "556e470fb06602a69831",
+    "url": "/JoelPoah.github.io/MedicationReact.git/static/js/2.1d916e84.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/JoelPoah.github.io/MedicationReact.git/static/js/2.1d916e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac58ec08f4ae12f34d06",
+    "url": "/JoelPoah.github.io/MedicationReact.git/static/js/main.f84ab9a9.chunk.js"
+  },
+  {
+    "revision": "144bc9b9e694c49007f5",
+    "url": "/JoelPoah.github.io/MedicationReact.git/static/js/runtime-main.5d975cd5.js"
+  }
+]);
