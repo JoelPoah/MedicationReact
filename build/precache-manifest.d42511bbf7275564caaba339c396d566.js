723e0bfe891e46d2e07e353b988b50f530d4b621
unknown <joel.21@ichat.sp.edu.sg>
2022-07-19 18:22:27 +0800

    initial commit

---
+++ build/precache-manifest.d42511bbf7275564caaba339c396d566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b70cc2f37a8606ce8dc62db620850ca",
+    "url": "/MedicationReact/index.html"
+  },
+  {
+    "revision": "ac58ec08f4ae12f34d06",
+    "url": "/MedicationReact/static/css/main.e6cdf681.chunk.css"
+  },
+  {
+    "revision": "556e470fb06602a69831",
+    "url": "/MedicationReact/static/js/2.1d916e84.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/MedicationReact/static/js/2.1d916e84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac58ec08f4ae12f34d06",
+    "url": "/MedicationReact/static/js/main.f84ab9a9.chunk.js"
+  },
+  {
+    "revision": "6cc5bb912bd90c33b3f1",
+    "url": "/MedicationReact/static/js/runtime-main.76d2235e.js"
+  }
+]);
